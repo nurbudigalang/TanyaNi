b46d19060c6565270c1f2c229519122c296e69de
jessicahidayat <jessica.hidayat@binus.ac.id>
2023-02-11 15:47:07 +0700

    test

--- website/views.py
+++ website/views.py
@@ -22,7 +22,8 @@ def errorPage():
 
 @views.route("/notification")
 def notification():
-    notifications = Notifikasi.query.filter_by(id_petani=current_user.id).order_by(Notifikasi.date.desc())
+    notifications = Notifikasi.query.filter_by(
+        id_petani=current_user.id).order_by(Notifikasi.date.desc())
     return render_template("notification.html", notifications=notifications)
 
 
@@ -34,24 +35,28 @@ def forgotPassword():
 @views.route("/pertanyaanku/<type>")
 def pertanyaanku(type):
     if type == "asc":
-        pertanyaan = Pertanyaan.query.filter_by(id_petani=current_user.id).order_by(Pertanyaan.date.asc())
+        pertanyaan = Pertanyaan.query.filter_by(
+            id_petani=current_user.id).order_by(Pertanyaan.date.asc())
     elif type == "desc":
-        pertanyaan = Pertanyaan.query.filter_by(id_petani=current_user.id).order_by(Pertanyaan.date.desc())
+        pertanyaan = Pertanyaan.query.filter_by(
+            id_petani=current_user.id).order_by(Pertanyaan.date.desc())
     return render_template("pertanyaanku.html", my_posts=pertanyaan)
 
 
 @views.route("/disimpan")
 def disimpan():
     # Mendapatkan daftar id pertanyaan yang dibookmark oleh user
-    id_pertanyaan_bookmarked = [bm.id_pertanyaan for bm in Bookmark.query.filter_by(id_petani=current_user.id)]
+    id_pertanyaan_bookmarked = [
+        bm.id_pertanyaan for bm in Bookmark.query.filter_by(id_petani=current_user.id)]
 
     # Menggunakan daftar id pertanyaan untuk memfilter data dari table "pertanyaan"
-    pertanyaan_bookmarked = Pertanyaan.query.filter(Pertanyaan.id.in_(id_pertanyaan_bookmarked))
+    pertanyaan_bookmarked = Pertanyaan.query.filter(
+        Pertanyaan.id.in_(id_pertanyaan_bookmarked))
 
     return render_template("disimpan.html", posts=pertanyaan_bookmarked, user=current_user)
 
 
-@views.route("/hapusProfil", methods=["POST"])
+@views.route("/hapusProfil", methods=["GET", "POST"])
 def hapusProfil():
     if request.method == "POST":
         db.session.delete(current_user)
@@ -69,7 +74,8 @@ def jawaban():
 @views.route("/detailPertanyaan/<id>", methods=["GET"])
 def detailPertanyaan(id):
     form = AnswerForm(request.form)
-    jawaban = Jawaban.query.filter_by(id_petani=current_user.id, id_pertanyaan=id).first()
+    jawaban = Jawaban.query.filter_by(
+        id_petani=current_user.id, id_pertanyaan=id).first()
     if jawaban:
         form.detail.data = jawaban.detail
     post = Pertanyaan.query.get(id)
@@ -79,7 +85,8 @@ def detailPertanyaan(id):
 
 @views.route("/editJawaban/<id>", methods=["GET", "POST"])
 def editJawaban(id):
-    answer = Jawaban.query.filter_by(id_petani=current_user.id, id_pertanyaan=id).first()
+    answer = Jawaban.query.filter_by(
+        id_petani=current_user.id, id_pertanyaan=id).first()
     form = AnswerForm(request.form, obj=answer)
     if request.method == "POST":
         answer.detail = form.detail.data
@@ -94,7 +101,8 @@ def tambahJawaban(id):
     if request.method == "POST":
         detail = form.detail.data
         user_id = current_user.id
-        new_answer = Jawaban(id_pertanyaan=id, id_petani=user_id, detail=detail)
+        new_answer = Jawaban(
+            id_pertanyaan=id, id_petani=user_id, detail=detail)
         db.session.add(new_answer)
         db.session.commit()
         notifikasi = Notifikasi(
